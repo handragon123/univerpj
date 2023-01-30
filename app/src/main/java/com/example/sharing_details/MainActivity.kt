c0accc03e2735534bfb69942cfebf1f487e8463c
Pjiyoung <pjy070401@naver.com>
2023-01-30 14:28:30 +0900

    sharing_details

---
+++ app/src/main/java/com/example/sharing_details/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sharing_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`sharing_details.xml`)
+    }
+}
