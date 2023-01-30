951ada5c33f182f205a336c1f38ca3817823c4d9
Pjiyoung <pjy070401@naver.com>
2023-01-30 12:09:52 +0900

    sign_up

---
+++ app/src/main/java/com/example/sharing_details/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sharing_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newpage.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`sing_up.xml`)
+    }
+}
