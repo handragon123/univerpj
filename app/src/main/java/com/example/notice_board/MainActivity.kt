de25a45d2d7a185d932ec14255b9a5dc164b0154
Pjiyoung <pjy070401@naver.com>
2023-01-30 13:00:41 +0900

    bulletin_board

---
+++ app/src/main/java/com/example/notice_board/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.notice_board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`bulletin_board.xml`)
+    }
+}
