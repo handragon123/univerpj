a5b9bbe2d25737977e05baba304bc5006d3d088a
Pjiyoung <pjy070401@naver.com>
2023-01-30 15:47:43 +0900

    pjy

---
+++ app/src/main/java/com/example/newpage/notice_board.kt
@@ -0,0 +1,11 @@
+package com.example.newpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class notice_board : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.notice_board)
+    }
+}
---
+++ app/src/main/java/com/example/newpage/sharing_details.kt
@@ -0,0 +1,11 @@
+package com.example.newpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sharing_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`sharing_details.xml`)
+    }
+}
